561e327b83b0210178844f709bcc03dd915ffde6
Soumyamanna123 <soumyamanna729@gmail.com>
2025-11-04 12:28:06 +0530

    route

--- src/app.js
+++ src/app.js
@@ -3,9 +3,27 @@
 
 const app = express()
 
-app.use("/hi",(req,res)=>{
-    res.send("hello0")
+
+app.get("/user/:userid", (req,res)=>{
+    console.log("user ID:", req.params)
+    res.send({firstname:"Soumuya", lastname:"Manna"})
 })
+
+app.post("/user",(req,res)=>{
+    res.send("Object saved to DB")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("ID Deleted")
+})
+
+
+
+app.use("/test",(req,res)=>{
+    res.send("Hello from the Server")
+})
+
+
 
 app.listen(3000, ()=>{
     console.log("success")
