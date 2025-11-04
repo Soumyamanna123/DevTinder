5c55a3b7de159bcbec96ebc9e118d1c42ed6a12f
Soumyamanna123 <soumyamanna729@gmail.com>
2025-11-04 15:26:21 +0530

    add

--- src/app.js
+++ src/app.js
@@ -1,33 +1,36 @@
- const express = require('express') 
+const express = require("express");
 
+const app = express();
 
-const app = express()
+// app.get("/user/:userid", (req,res)=>{
+//     console.log("user ID:", req.params)
+//     res.send({firstname:"Soumuya", lastname:"Manna"})
+// })
 
+// app.post("/user",(req,res)=>{
+//     res.send("Object saved to DB")
+// })
 
-app.get("/user/:userid", (req,res)=>{
-    console.log("user ID:", req.params)
-    res.send({firstname:"Soumuya", lastname:"Manna"})
-})
+// app.delete("/user",(req,res)=>{
+//     res.send("ID Deleted")
+// })
 
-app.post("/user",(req,res)=>{
-    res.send("Object saved to DB")
-})
+app.use(
+  "/user",[
+  (req, res, next) => {
+       next();
+    res.send("i am from handler one");
+ 
+  }],
+  (req, res) => {
+    res.send("i am from handler two");
+  }
+);
 
-app.delete("/user",(req,res)=>{
-    res.send("ID Deleted")
-})
+app.use("/test", (req, res) => {
+  res.send("Hello from the Server");
+});
 
-
-
-app.use("/test",(req,res)=>{
-    res.send("Hello from the Server")
-})
-
-
-
-app.listen(3000, ()=>{
-    console.log("success")
-})
-
-
-
+app.listen(3000, () => {
+  console.log("success");
+});
